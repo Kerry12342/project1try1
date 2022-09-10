49eef5638c62ba6fe1cef7c2b57131d5466746ab
Kerry Wang <kerrywang@kerrys-mbp.hamilton.edu>
2022-09-10 19:34:29 -0400

    commit the file into the github repo

---
+++ Project0.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+int main()
+{
+   int goKerry = 1;
+   char boringMe = 'a';
+   std::string myCode [] = {
+      "#include <iostream>",
+      "int main()",
+      "{",
+      "   int goKerry = 1;",
+      "   char boringMe = 'a';",
+      "   std::string myCode [] = {",
+      "}",
+      "   for (int index = 0; index < 6; index++){std::cout<<myCode[index]<<std::endl;}",
+      "   for (int index = 0; index < 15; index++){for(int times = 0; times < 6; times++){std::cout<<char(32);}std::cout<<char(34)<< myCode[index]<<char(34)<<char(44)<<std::endl;}",
+      "   for(int times = 0; times < 6; times++){std::cout<<char(32);}",
+      "   std::cout<<char(34)<<myCode[15]<<char(34)<<std::endl;",
+      "   for(int times = 0; times < 3; times++){std::cout<<char(32);}",
+      "   std::cout<<myCode[6]<<char(59)<<std::endl;",
+      "   for (int index = 7; index < 16; index++){std::cout<<myCode[index]<<std::endl;}",
+      "   return 0;",
+      "}"
+   };
+   for (int index = 0; index < 6; index++){std::cout<<myCode[index]<<std::endl;}
+   for (int index = 0; index < 15; index++){for(int times = 0; times < 6; times++){std::cout<<char(32);}std::cout<<char(34)<< myCode[index]<<char(34)<<char(44)<<std::endl;}
+   for(int times = 0; times < 6; times++){std::cout<<char(32);}
+   std::cout<<char(34)<<myCode[15]<<char(34)<<std::endl;
+   for(int times = 0; times < 3; times++){std::cout<<char(32);}
+   std::cout<<myCode[6]<<char(59)<<std::endl;
+   for (int index = 7; index < 16; index++){std::cout<<myCode[index]<<std::endl;}
+   return 0;
+}
